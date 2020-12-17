d728991446faa763a8d66ab026eee2ba44e443b3
egorlichenkoam <egorlichenkoam@gmail.com>
2020-12-17 14:48:46 +0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	var balance int32 = 1500000000  // 15 миллионов в копейках
+	var transfer int32 = 1000000000 // 10 миллионов в копейках
+	total := balance + transfer     // int32 + int32 будет int32
+	println(total)
+}
